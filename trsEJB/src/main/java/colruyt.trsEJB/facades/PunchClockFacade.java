0b2090fd1292cb08386a9ad2c7142aa0449c94aa
Timur <vikiplayground@gmail.com>
2018-10-28 15:48:49 +0100

    punchclockfade change

--- trsEJB/src/main/java/colruyt.trsEJB/facades/PunchClockFacade.java
+++ trsEJB/src/main/java/colruyt.trsEJB/facades/PunchClockFacade.java
@@ -2,8 +2,6 @@ package colruyt.trsEJB.facades;
 
 import colruyt.trsEJB.bo.PersonBo;
 import colruyt.trsEJB.bo.ProjectBo;
-
-import colruyt.trsEJB.util.exceptions.CredentialNotFoundException;
 import colruyt.trsEJB.util.exceptions.OutsideWorkdayTimeBoundariesException;
 
 import java.time.LocalDateTime;
@@ -15,12 +13,6 @@ public interface PunchClockFacade {
             LocalDateTime start, LocalDateTime end, String activity, ProjectBo project
     ) throws OutsideWorkdayTimeBoundariesException;
 
-    void punch(String persId, String password) throws CredentialNotFoundException;
+    void punch(PersonBo personBo) ;
 
-    void punch(PersonBo personBo) throws CredentialNotFoundException;
-
-
-    // PersonBo authenticate(String persId, String password) throws CredentialNotFoundException;
-
-    // PersonBo getAuthenticatedPerson() throws CredentialNotFoundException;
 }
--- trsEJB/src/main/java/colruyt.trsEJB/facades/PunchClockFacadeImpl.java
+++ trsEJB/src/main/java/colruyt.trsEJB/facades/PunchClockFacadeImpl.java
@@ -5,15 +5,14 @@ import colruyt.trsEJB.bo.ProjectBo;
 import colruyt.trsEJB.services.bl.ActivityService;
 import colruyt.trsEJB.services.bl.PersonService;
 import colruyt.trsEJB.services.bl.TimePunchService;
-import colruyt.trsEJB.util.exceptions.CredentialNotFoundException;
 import colruyt.trsEJB.util.exceptions.OutsideWorkdayTimeBoundariesException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 @Stateless
 @LocalBean
@@ -25,15 +24,17 @@ public class PunchClockFacadeImpl implements PunchClockFacade {
 
     /* SERVICES */
 
-    // TODO: Inject
-    private PersonService personService = PersonService.getService();
+    @EJB
+    private PersonService personService;
 
+    @EJB
+    private TimePunchService timePunchService;
 
-    private TimePunchService timePunchService = TimePunchService.getService();
-
-    private ActivityService activityService = ActivityService.getService();
+    @EJB
+    private ActivityService activityService;
 
     /* CONSTRUCTORS */
+
     public PunchClockFacadeImpl() { LOGGER.trace("PunchClockFacade constructed"); }
 
     /* PUBLIC ENTRIES */
@@ -56,55 +57,8 @@ public class PunchClockFacadeImpl implements PunchClockFacade {
     }
 
     @Override
-    public void punch(String persId, String password) throws CredentialNotFoundException {
-
-        LOGGER.traceEntry( "punch( PersID = {}, Password = {} )", persId, password );
-        //TODO: Zie Authenticate return ( dubbele assign);
-        authenticatedPerson = authenticate(persId, password);
-
-        try {
-            LOGGER.info(
-                    "passing the 'punch' to persistence layer for persID: {}",
-                    persId
-            );
-            timePunchService.punch( authenticatedPerson );
-        }
-        catch (Exception e) {
-            LOGGER.fatal("CredentialNotFoundException");
-            LOGGER.catching(e);
-            // TODO: Exception handling?
-        }
-        LOGGER.info("Punch was accepted for the persID: {}", persId);
-
-        LOGGER.traceExit("{}","punch()");
+    public void punch(PersonBo personBo) {
+        timePunchService.punch(personBo);
     }
 
-    @Override
-    public PersonBo authenticate(String persId, String password) throws CredentialNotFoundException {
-        LOGGER.traceEntry(
-                "Authenticating persID: {}", persId
-        );
-
-        // query person => is never Null => will be empty obj: Optional if no person was found
-        Optional<PersonBo> optionalPersonBO = personService.getByCredentials(persId, password);
-
-        LOGGER.info("Optional object = '{}' was provided by the service", optionalPersonBO);
-
-        // if no person was found => Exception : CredentialNotFoundException
-        if ( !optionalPersonBO.isPresent() ) {
-
-            LOGGER.warn("Object Optional<Person> is empty");
-
-            throw new CredentialNotFoundException();
-        }
-        return this.authenticatedPerson = optionalPersonBO.get();
-    }
-
-    @Override
-    public PersonBo getAuthenticatedPerson() throws CredentialNotFoundException {
-        if(this.authenticatedPerson == null) {
-            throw new CredentialNotFoundException();
-        }
-        return this.authenticatedPerson;
-    }
 }
