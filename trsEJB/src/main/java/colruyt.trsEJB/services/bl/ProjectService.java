966e9e1a590fd2e1991bbe7a0bc786c09bbcaf09
Timur <vikiplayground@gmail.com>
2018-10-28 17:17:49 +0100

    service

--- trsEJB/src/main/java/colruyt.trsEJB/services/bl/ProjectService.java
+++ trsEJB/src/main/java/colruyt.trsEJB/services/bl/ProjectService.java
@@ -1,7 +1,28 @@
 package colruyt.trsEJB.services.bl;
 
+import colruyt.trsEJB.bo.ProjectBo;
+import colruyt.trsEJB.services.dl.PersonDlService;
+
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 @Stateless
 public class ProjectService {
+
+    @EJB
+    PersonDlService personDlService;
+
+    public ProjectBo getById() {
+
+        // TODO: 2implement
+        return null;
+    }
+
+    public ProjectBo getByName() {
+
+        // TODO: 2implement
+        return null;
+    }
+
+
 }
